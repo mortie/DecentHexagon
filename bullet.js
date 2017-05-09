567b9241ffc6e9a3b97787f1ec989749e5b1fbcd
mortie <martid0311@gmail.com>
2017-05-09 20:09:52 +0200

    fix yo indentation, and made camera work

--- bullet.js
+++ bullet.js
@@ -1,5 +1,5 @@
 function makeBullet(x, y, vely, velx, shooterType) {
-  var bullet = new Object();
+    var bullet = new Object();
     bullet.size = 20;
     bullet.hexOut = Math.round(Math.sqrt((bullet.size*bullet.size)-(((1.72*bullet.size)/2)*((1.72*bullet.size)/2))));
     bullet.height = Math.round((1.72*bullet.size));
@@ -27,109 +27,109 @@ function makeBullet(x, y, vely, velx, shooterType) {
     bullet.dead = false;
 
     bullet.startExploding = function() {
-      this.vely= 0;
-      this.velx = 0;
-      this.a = 0;
-      this.canExplode = true;
-      setTimeout(function(i) {
-        this.canExplode = false;
-        this.dead = true;
-      }.
-      bind(this), 300);
-      this.hasExploded = true;
+        this.vely= 0;
+        this.velx = 0;
+        this.a = 0;
+        this.canExplode = true;
+        setTimeout(function(i) {
+            this.canExplode = false;
+            this.dead = true;
+        }.
+            bind(this), 300);
+        this.hasExploded = true;
     }
 
     bullet.makeBoom = function() {
-      if (this.canExplode == true) {
-        if (this.isExploding == false) {
-          this.isExploding = true;
-          setTimeout(function() {
-            this.isExploding = false;
-          }.bind(this), 300);
+        if (this.canExplode == true) {
+            if (this.isExploding == false) {
+                this.isExploding = true;
+                setTimeout(function() {
+                    this.isExploding = false;
+                }.bind(this), 300);
+            }
         }
-      }
     };
 
     bullet.stop = function() {
         //rightBoom
         if (x + bullet.size +bullet.hexOut > canvas.width) {
-          x = canvas.width - this.size - this.hexOut - 1;
-          this.velx = 0;
-          this.startExploding();
-          this.hasExploded = true;
+            x = canvas.width - this.size - this.hexOut - 1;
+            this.velx = 0;
+            this.startExploding();
+            this.hasExploded = true;
         }
         //leftBoom
         if (x - this.hexOut < 0) {
-          x = this.hexOut + 1;
-          this.velx = 0;
-          this.startExploding();
-          this.hasExploded = true;
+            x = this.hexOut + 1;
+            this.velx = 0;
+            this.startExploding();
+            this.hasExploded = true;
         }
         //stahp
         if (this.velx < 0) {this.velx += 0; }
         if (this.velx > 0) {this.velx -= 0; }
         //falling
         if (y + this.height < canvas.height) {
-          this.vely+= 0.3;
+            this.vely+= 0.3;
         }
         //groundBoom
         if (y+this.Halfheight > canvas.height) {
-          y = canvas.height - this.height + bullet.Halfheight;
-          this.startExploding();
-          this.hasExploded = true;
+            y = canvas.height - this.height + bullet.Halfheight;
+            this.startExploding();
+            this.hasExploded = true;
         }
         //probNotGoingToApplyButRoofBoomJustInCase
         if (y < 0) {
-          y = 0;
-          this.vely= 0;
-          this.startExploding();
-          this.hasExploded = true;
+            y = 0;
+            this.vely= 0;
+            this.startExploding();
+            this.hasExploded = true;
         }
     };
 
     bullet.update = function() {
-      bullet.y = y;
-      bullet.x = x;
-      this.vely+= vely;
-      this.velx+= velx;
-      x += Math.round(this.velx);
-      y += this.vely;
-      vely= 0;
-      velx= 0;
-      bullet.stop();
+        bullet.y = y;
+        bullet.x = x;
+        this.vely+= vely;
+        this.velx+= velx;
+        x += Math.round(this.velx);
+        y += this.vely;
+        vely= 0;
+        velx= 0;
+        bullet.stop();
 
-      if (this.hasExploded) {
-        this.dx = this.x-this.dWidth/2+this.size/2;
-        this.dy = this.y-this.dHeight/2;
-        ctx.drawImage(explosion, this.sx, this.sy, this.sWidth, this.sHeight, this.dx, this.dy, this.dWidth, this.dHeight);
-      }
+        if (this.hasExploded) {
+            this.dx = this.x-this.dWidth/2+this.size/2;
+            this.dy = this.y-this.dHeight/2;
+            ctx.drawImage(explosion, this.sx, this.sy, this.sWidth, this.sHeight, this.dx, this.dy, this.dWidth, this.dHeight);
+        }
     };
 
     bullet.draw = function() {
-      ctx.beginPath();
-      ctx.moveTo(x, y-bullet.Halfheight);
-      ctx.lineTo(x+bullet.size, y-bullet.Halfheight);
-      ctx.lineTo(x+bullet.size+bullet.hexOut, y);
-      ctx.lineTo(x+bullet.size, y+bullet.height-bullet.Halfheight);
-      ctx.lineTo(x, y+bullet.height-bullet.Halfheight);
-      ctx.lineTo(x-bullet.hexOut, y);
-      ctx.lineTo(x, y-bullet.Halfheight);
-      var b = Math.floor(Math.abs(Math.sin(x / 200)) * 255);
-      var g = Math.floor(Math.abs(Math.sin(y / 200)) * 255);
-      ctx.fillStyle = "rgba(0, "+g+", "+b+", "+this.a+")";
-      ctx.fill();
-      bullet.makeBoom();
+        ctx.beginPath();
+        ctx.moveTo(x, y-bullet.Halfheight);
+        ctx.lineTo(x+bullet.size, y-bullet.Halfheight);
+        ctx.lineTo(x+bullet.size+bullet.hexOut, y);
+        ctx.lineTo(x+bullet.size, y+bullet.height-bullet.Halfheight);
+        ctx.lineTo(x, y+bullet.height-bullet.Halfheight);
+        ctx.lineTo(x-bullet.hexOut, y);
+        ctx.lineTo(x, y-bullet.Halfheight);
+        var b = Math.floor(Math.abs(Math.sin(x / 200)) * 255);
+        var g = Math.floor(Math.abs(Math.sin(y / 200)) * 255);
+        ctx.fillStyle = "rgba(0, "+g+", "+b+", "+this.a+")";
+        ctx.fill();
+        bullet.makeBoom();
     };
 
     setInterval(animateExplosion, 16)
-      function animateExplosion() {
+    function animateExplosion() {
         if (bullet.canExplode == true) {
-        if (bullet.sx < 1824) {
-          bullet.sx += 96
+            if (bullet.sx < 1824) {
+                bullet.sx += 96
+            }
+            else {bullet.sx = 96}
         }
-        else {bullet.sx = 96}
-        }
-      }
+    }
 
     return bullet;
-  }
+}
--- vector.js
+++ vector.js
@@ -1,49 +1,49 @@
 function Vec2(x, y) {
-  this.x = x || 0;
-  this.y = y || 0;
+    this.x = x || 0;
+    this.y = y || 0;
 }
 
 Vec2.prototype.clone = function clone() {
-  return new Vec2(this.x, this.y);
+    return new Vec2(this.x, this.y);
 };
 
 Vec2.prototype.length = function length() {
-  return Math.sqrt((this.x*this.x)+(this.y*this.y));
+    return Math.sqrt((this.x*this.x)+(this.y*this.y));
 };
 
 Vec2.prototype.add = function add(smallVec) {
-  this.x += smallVec.x;
-  this.y += smallVec.y;
-  return this;
+    this.x += smallVec.x;
+    this.y += smallVec.y;
+    return this;
 };
 
 Vec2.prototype.sub = function sub(smallVec) {
-  this.x -= smallVec.x;
-  this.y -= smallVec.y;
-  return this;
+    this.x -= smallVec.x;
+    this.y -= smallVec.y;
+    return this;
 };
 
 Vec2.prototype.scale = function scale(burrito) {
-  this.x *= burrito;
-  this.y *= burrito;
-  return this;
+    this.x *= burrito;
+    this.y *= burrito;
+    return this;
 };
 
 Vec2.prototype.normalize = function normalize() {
-  if (this.x == 0 && this.y == 0) {
-    this.x = 0;
-    this.y = -1;
-  }
-  else {
-    this.scale(1/this.length());
-  }
-  return this;
+    if (this.x == 0 && this.y == 0) {
+        this.x = 0;
+        this.y = -1;
+    }
+    else {
+        this.scale(1/this.length());
+    }
+    return this;
 };
 
 Vec2.prototype.rotate = function rotate(radical) {
-  var x = this.x;
-  var y = this.y;
-  this.x = x * Math.cos(radical) - y * Math.sin(radical);
-  this.y = y * Math.cos(radical) - x * Math.sin(radical);
-  return this;
+    var x = this.x;
+    var y = this.y;
+    this.x = x * Math.cos(radical) - y * Math.sin(radical);
+    this.y = y * Math.cos(radical) - x * Math.sin(radical);
+    return this;
 };
